72af0c44046d0717e96b21a251ae8327691c6d85
softpromani <softpromani@gmail.com>
2024-12-20 12:55:28 +0530

    bug fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \RealRashid\SweetAlert\ToSweetAlert::class,
         ]);
+
+        // Using a closure...
+        $middleware->redirectGuestsTo(fn (Request $request) => route('home'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
