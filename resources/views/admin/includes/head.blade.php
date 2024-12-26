2007912d1b1e0ada8586935f5e3c1221496d251a
softpromani <softpromani@gmail.com>
2024-12-26 11:29:10 +0530

    bug fixes

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -5,8 +5,8 @@
 <meta content="" name="keywords">
 
 <!-- Favicons -->
-<link href="assets/img/favicon.png" rel="icon">
-<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="{{ asset('admin/assets/img/bam_logo.png') }}" rel="icon">
+<link href="{{ asset('admin/assets/img/bam_logo.png') }}" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link href="https://fonts.gstatic.com" rel="preconnect">
