f44f01c636448a23189876aaecf8c7c4b81a0d15
softpromani <softpromani@gmail.com>
2024-12-28 17:17:33 +0530

    bug fixes

--- app/Console/Commands/FetchAuthorityRates.php
+++ app/Console/Commands/FetchAuthorityRates.php
@@ -6,36 +6,36 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use App\Models\BusinessSetting;
 
-class FetchAuthorityRatesCommand extends Command
+class FetchAuthorityRates extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'fetch:authority-rates';
+    protected $signature = 'rates:fetch-authority';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Fetches authority rates and updates business settings';
+    protected $description = 'Fetch authority rates from API and update the database';
 
     /**
      * Execute the console command.
+     *
+     * @return int
      */
     public function handle()
     {
         $apiUrl = 'https://api.metals.dev/v1/metal/authority';
         $apiKey = '5YZQ9MIBTU622GNCQR8V103NCQR8V';
 
-        // Parameters
         $authority = 'mcx';
         $currency = 'INR';
         $unit = 'g';
 
-        // Make the API request
         $response = Http::get($apiUrl, [
             'api_key' => $apiKey,
             'authority' => $authority,
@@ -43,17 +43,17 @@ class FetchAuthorityRatesCommand extends Command
             'unit' => $unit,
         ]);
 
-        // Handle the API response
         if ($response->successful()) {
             $responseData = $response->json();
 
-            // Update the business settings
             BusinessSetting::updateOrCreate(['key' => 'mcx_gold_rate'], ['value' => $responseData['rates']['mcx_gold']]);
             BusinessSetting::updateOrCreate(['key' => 'mcx_silver_rate'], ['value' => $responseData['rates']['mcx_silver']]);
 
-            $this->info('Authority rates fetched and updated successfully.');
+            $this->info('Authority rates updated successfully.');
         } else {
             $this->error('Failed to fetch authority rates. Error: ' . $response->body());
         }
+
+        return 0;
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('rates:fetch-authority', function () {
+    $this->call('rates:fetch-authority');
+})->describe('Fetch authority rates')->schedule(['--dailyAt' => ['14:00', '17:00']]);
