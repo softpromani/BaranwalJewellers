10ca7c7c448c492f5d0450b101e2b71bd6c8d19f
softpromani <softpromani@gmail.com>
2024-12-28 17:27:56 +0530

    bug fixes

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -10,11 +10,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Notification extends Model
 {
     use HasFactory, SoftDeletes;
-  protected $guarded = [];
-  protected $append = ['new_date_time'];
 
-  function getNewDateTimeAttribute()
-  {
-    return Carbon::parse($this->created_at)->format('d M Y, HH:ii');
-  }
+    protected $guarded = [];
+    protected $appends = ['new_date_time'];
+
+    /**
+     * Accessor for new_date_time.
+     *
+     * @return string
+     */
+    public function getNewDateTimeAttribute()
+    {
+        return Carbon::parse($this->created_at)->format('d M Y, h:i A');
+    }
 }
